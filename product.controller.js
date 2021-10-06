42a0d6661802ced3343af039ce888272307a494e
growebux <70495348+growebux@users.noreply.github.com>
2021-10-05 23:38:18 -0500

    Add files via upload

---
+++ product.controller.js
@@ -0,0 +1,23 @@
+const Product = require('../models/product.model');
+
+//Simple version, without validation or sanitation
+exports.test = function (req, res) {
+    res.send('Greetings from the Test cggggontroller!');
+};
+
+// controllers/products.js
+exports.product_create = function (req, res) {
+    let product = new Product(
+        {
+            name: req.body.name,
+            price: req.body.price
+        }
+    );
+
+    product.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.send('Product Created successfully')
+    })
+};
---
+++ product.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let ProductSchema = new Schema({
+    name: {type: String, required: true, max: 100},
+    price: {type: Number, required: true},
+});
+
+
+// Export the model
+module.exports = mongoose.model('Product', ProductSchema);
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://eddie:1N8utt7IrFZyNy9P@ds123619.mlab.com:23619/Cluster0';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
